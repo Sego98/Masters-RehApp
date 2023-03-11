6ec5616f4f7114d16e3ab4a771422e0504c11e8d
Sego98 <petar.ljubotina@fer.hr>
2023-03-11 20:01:16 +0100

    Add colors

---
+++ RehApp/Visual design/UIColor.swift
@@ -0,0 +1,26 @@
+//
+//  UIColor.swift
+//  RehApp
+//
+//  Created by Akademija on 11.03.2023..
+//
+
+import UIKit
+
+extension UIColor {
+
+    /// light: #F2EEE2, dark: #00072D
+    static let rehAppBackground = UIColor(named: "RehAppBackground")
+    
+    /// #8B0404
+    static let burgundy = UIColor(named: "Burgundy")
+    
+    /// #BE95C4
+    static let lightPurple = UIColor(named: "LightPurple")
+    
+    /// #9F86C0
+    static let darkPurple = UIColor(named: "DarkPurple")
+    
+    /// light: #272E63, dark: #F7FFE0
+    static let rehAppBase = UIColor(named: "RehAppBase")
+}
