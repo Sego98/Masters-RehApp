edff07d99a020dcf6f1be18e21f28f8b05b13583
Sego98 <petar.ljubotina@fer.hr>
2023-05-01 20:13:22 +0200

    Localize shoulder exercises with stick

--- RehApp/Screens/Exercises/ExerciseDetails/ExerciseDetailsVM.swift
+++ RehApp/Screens/Exercises/ExerciseDetails/ExerciseDetailsVM.swift
@@ -78,67 +78,49 @@ struct ExerciseDetailsVM: Hashable, Equatable {
     // MARK: - Shoulder exercises with stick
 
     static let shoulderExerciseWithStick1: ExerciseDetailsVM = {
-        return ExerciseDetailsVM(title: "Podizanje štapa",
-                                 shortDescription: "Primiti štap u širini ramena",
-                                 longDescription: """
-                Primite štap s opuštenim ispruženim rukama u širini ramena. Štap podižite prema gore \
-                koliko god je moguće. Cilj vježbe je da se štap može spustiti na vrat iza glava, ali \
-                to se ne smije forsirati!
-                """,
+        return ExerciseDetailsVM(title: "StickLifting".localize(),
+                                 shortDescription: "StickShoulderWidth".localize(),
+                                 longDescription: "ShoulderExerciseWithStickDescription1".localize(),
                                  oneRepetitionTime: mockOneRepetitionTime,
                                  videoURL: mockURL)
     }()
 
     static let shoulderExerciseWithStick2: ExerciseDetailsVM = {
-        return ExerciseDetailsVM(title: "Podizanje štapa",
-                                 shortDescription: "Primiti štap na krajevima",
-                                 longDescription: """
-                Primite štap s opuštenim ispruženim rukama na krajevima. Štap podižite prema gore \
-                koliko god je moguće. Cilj vježbe je da se štap može spustiti na vrat iza glava, ali \
-                to se ne smije forsirati!
-                """,
+        return ExerciseDetailsVM(title: "StickLifting".localize(),
+                                 shortDescription: "GrabStickEnd".localize(),
+                                 longDescription: "ShoulderExerciseWithStickDescription2".localize(),
                                  oneRepetitionTime: mockOneRepetitionTime,
                                  videoURL: mockURL)
     }()
 
     static let shoulderExerciseWithStick3: ExerciseDetailsVM = {
-        return ExerciseDetailsVM(title: "Povlačenje štapa",
-                                 shortDescription: "Primiti štap u visini ramena i povlačiti prema bradi",
-                                 longDescription: """
-                Primite štap s ispruženim rukama u visini ramena. Štap držite u tom položaju i privlačite \
-                ga prema bradi. Lakat i rame cijelo vrijeme ostaju u istoj visini.
-                """,
+        return ExerciseDetailsVM(title: "PullingStick".localize(),
+                                 shortDescription: "PullingStickShoulderHeight".localize(),
+                                 longDescription: "ShoulderExerciseWithStickDescription3".localize(),
                                  oneRepetitionTime: mockOneRepetitionTime,
                                  videoURL: mockURL)
     }()
 
     static let shoulderExerciseWithStick4: ExerciseDetailsVM = {
-        return ExerciseDetailsVM(title: "Istezanje zasucima",
-                                 shortDescription: "Primiti štap na krajevima i istezati se rotacijama",
-                                 longDescription: """
-                Primite štap na krajeve s ispruženim rukama u visini ramena. Radite zasuke u lijevu i desnu stranu \
-                kako biste istegnuli leđa. Jedan lakat se savija, a drugi se pruža u stranu.
-                """,
+        return ExerciseDetailsVM(title: "StretchingWithCurls".localize(),
+                                 shortDescription: "StickEndRotate".localize(),
+                                 longDescription: "ShoulderExerciseWithStickDescription4".localize(),
                                  oneRepetitionTime: mockOneRepetitionTime,
                                  videoURL: mockURL)
     }()
 
     static let shoulderExerciseWithStick5: ExerciseDetailsVM = {
-        return ExerciseDetailsVM(title: "Štap iza leđa",
-                                 shortDescription: "Primiti štap iza leđa i odmicati ga",
-                                 longDescription: """
-                Primite štap iza leđa u donjem dijelu i odmičite ga od sebe što je više moguće
-                """,
+        return ExerciseDetailsVM(title: "StickBehindBack".localize(),
+                                 shortDescription: "StickBehindBackMoveAway".localize(),
+                                 longDescription: "ShoulderExerciseWithStickDescription5".localize(),
                                  oneRepetitionTime: mockOneRepetitionTime,
                                  videoURL: mockURL)
     }()
 
     static let shoulderExerciseWithStick6: ExerciseDetailsVM = {
-        return ExerciseDetailsVM(title: "Štap iza leđa",
-                                 shortDescription: "Primiti štap iza leđa i povlačiti ga",
-                                 longDescription: """
-                Primite štap iza leđa u donjem dijelu i povlačite ga prema lopaticama što je više moguće
-                """,
+        return ExerciseDetailsVM(title: "StickBehindBack".localize(),
+                                 shortDescription: "StickBehindBackPull".localize(),
+                                 longDescription: "ShoulderExerciseWithStickDescription6".localize(),
                                  oneRepetitionTime: mockOneRepetitionTime,
                                  videoURL: mockURL)
     }()
